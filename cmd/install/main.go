056d8fc3a667fa796ce98aa7af517475ffacff36
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-05-29 13:24:56 -0500

    Backup:

--- cmd/install/main.go
+++ cmd/install/main.go
@@ -8,13 +8,25 @@ import (
 var dependencies = []string{
 	"github.com/joho/godotenv@v1.5.1",
 	"github.com/bwmarrin/snowflake@v0.3.0",
-	"github.com/google/uuid@v1.6.0",
+	"github.com/oklog/ulid/v2@v2.1.1",
 	"github.com/matoous/go-nanoid/v2@v2.1.0",
 	"github.com/oklog/ulid@v1.3.1",
-	"golang.org/x/crypto/bcrypt@v0.37.0",
+	"github.com/lib/pq@v1.10.9",
+	"github.com/go-chi/chi@v1.5.5",
+	"github.com/go-chi/chi/v5@v5.2.2",
+	"github.com/google/uuid@v1.6.0",
+	"golang.org/x/crypto@v0.37.0",
+	"golang.org/x/exp@v0.0.0-20250408133849-7e4ce0ab07d0",
 	"github.com/manifoldco/promptui@v0.9.0",
 	"github.com/schollz/progressbar/v3@v3.18.0",
+	"github.com/redis/go-redis/v9@v9.12.1",
 	"github.com/spf13/cobra@v1.9.1",
+	"github.com/nats-io/nats.go@v1.41.2",
+	"github.com/golang-jwt/jwt/v4@v4.5.2",
+	"github.com/robfig/cron/v3@v3.0.1",
+	"github.com/rs/cors@v1.11.1",
+	"github.com/rs/xid@v1.6.0",
+	"github.com/go-sql-driver/mysql@v1.9.3",
 }
 
 func main() {

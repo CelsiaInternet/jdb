427630a46cfa440f679089ff6818ec436be6020b
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-05-29 11:32:26 -0500

    Backup:

--- drivers/sqlite/database.go
+++ drivers/sqlite/database.go
@@ -4,10 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"strings"
-	"time"
 
 	"github.com/celsiainternet/elvis/console"
-	"github.com/celsiainternet/elvis/envar"
 	jdb "github.com/celsiainternet/jdb/jdb"
 )
 
@@ -44,14 +42,14 @@ func (s *SqlLite) Connect(connection jdb.ConnectParams) (*sql.DB, error) {
 		return nil, err
 	}
 
-	maxOpenConns := envar.GetInt(3, "DB_MAX_OPEN_CONNS")
-	maxIdleConns := envar.GetInt(2, "DB_MAX_IDLE_CONNS")
-	connMaxLifetime := time.Duration(envar.GetInt(30, "DB_CONN_MAX_LIFETIME")) * time.Minute
-	connMaxIdleTime := time.Duration(envar.GetInt(5, "DB_CONN_MAX_IDLE_TIME")) * time.Minute
-	db.SetMaxOpenConns(maxOpenConns)
-	db.SetMaxIdleConns(maxIdleConns)
-	db.SetConnMaxLifetime(connMaxLifetime)
-	db.SetConnMaxIdleTime(connMaxIdleTime)
+	// maxOpenConns := envar.GetInt(3, "DB_MAX_OPEN_CONNS")
+	// maxIdleConns := envar.GetInt(2, "DB_MAX_IDLE_CONNS")
+	// connMaxLifetime := time.Duration(envar.GetInt(30, "DB_CONN_MAX_LIFETIME")) * time.Minute
+	// connMaxIdleTime := time.Duration(envar.GetInt(5, "DB_CONN_MAX_IDLE_TIME")) * time.Minute
+	// db.SetMaxOpenConns(maxOpenConns)
+	// db.SetMaxIdleConns(maxIdleConns)
+	// db.SetConnMaxLifetime(connMaxLifetime)
+	// db.SetConnMaxIdleTime(connMaxIdleTime)
 
 	s.connected = db != nil
 	console.LogKF(s.name, `Connected to %s`, database)
